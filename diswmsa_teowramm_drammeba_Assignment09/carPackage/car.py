514b2b4f5e18cc4f48f2c90984078388962b9dee
smita <smita@DESKTOP-L3NU2O8.lan>
2024-04-02 09:07:50 -0400

    second commit

--- diswmsa_teowramm_drammeba_Assignment09/carPackage/car.py
+++ diswmsa_teowramm_drammeba_Assignment09/carPackage/car.py
@@ -1,19 +1,6 @@
 <<<<<<< HEAD
 #car.py
 
-def carInfo(self, model):
-        apiUrl = f'https://api.api-ninjas.com/v1/cars?model={model}'
-        response = requests.get(apiUrl, headers={'X-Api-Key': self.apiKey})
-        if response.status_code == requests.codes.ok:
-            return json.loads(response.content)
-        #https://www.geeksforgeeks.org/json-loads-in-python/
-        #https://www.geeksforgeeks.org/how-to-parse-data-from-json-into-python/?ref=ml_lbp
-        #https://www.geeksforgeeks.org/convert-json-to-dictionary-in-python/?ref=ml_lbp
-        else:
-            return None
-=======
-# car.py
-
 import requests
 import json
 
@@ -27,7 +14,7 @@ class Car:
             'chevrolet': 'https://api.api-ninjas.com/v1/cars?model=chevrolet'
         }
         self.carModels = list(self.modelUrls.keys()) #getting dictionary keys from modelUrls dictionary
-          def carInfo(self, model):
+        def carInfo(self, model):
         apiUrl = f'https://api.api-ninjas.com/v1/cars?model={model}'
         response = requests.get(apiUrl, headers={'X-Api-Key': self.apiKey})
         if response.status_code == requests.codes.ok:
