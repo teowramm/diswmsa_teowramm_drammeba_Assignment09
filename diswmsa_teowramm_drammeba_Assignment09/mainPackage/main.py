9461fa844dd4ac281ea1eb08ce30e163e5875bd4
18594 <18594@DESKTOP-PQU7KFU.wifi.uc.edu>
2024-04-02 13:32:56 -0400

    commit

--- diswmsa_teowramm_drammeba_Assignment09/mainPackage/main.py
+++ diswmsa_teowramm_drammeba_Assignment09/mainPackage/main.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
     
     apiKey = 'VbX0aQXRm85Hr/exSWeRlA==z1tq1gt6z3OUe1oc' # API key needed for access
     carApi = Car(apiKey) # constructor call of Car class
+    model = input("Please enter your car model! : ")
+    carData = carApi.carInfo(model) # calling CarInfo method, connecting api key to URL
